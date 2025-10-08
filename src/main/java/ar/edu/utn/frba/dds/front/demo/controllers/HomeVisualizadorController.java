9829b5cfe45256c5fce28cc43976ad1b6f23d7e8
FrancoPadovani <fpadovani@frba.utn.edu.ar>
2025-10-08 08:45:21 -0300

    j

---
+++ src/main/java/ar/edu/utn/frba/dds/front/demo/controllers/HomeVisualizadorController.java
@@ -0,0 +1,13 @@
+package ar.edu.utn.frba.dds.front.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeVisualizadorController {
+
+    @GetMapping("/")
+    public String home() {
+        return "visualizador/home";
+    }
+}
